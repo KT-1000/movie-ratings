840c7b943a050a16b02539a489ca9a40f8912e3f
Hackbright Student <info@hackbrightacademy.com>
2016-05-05 17:25:31 -0700

    Working application

--- server.py
+++ server.py
@@ -48,14 +48,80 @@ def movie_details(movie_id):
         # current_user_rating = (rating_id, movie_id, user_id, score)
         current_user_rating = (db.session.query(Rating)
         .filter(Rating.user_id == current_user_id, Rating.movie_id == movie_id).first())
-    # If user isn't logged in, redirect to login page
-    # else:
-    #     flash("Please log in to rate a movie.")
 
     movie_ratings = (db.session.query(Rating.score, Rating.user_id).filter(Rating.movie_id==movie_id).all())
 
-    return render_template("movie_details.html", movie=movie, movie_ratings=movie_ratings, 
-        current_user_id=current_user_id, current_user_rating=current_user_rating)
+    # Get average rating of movie
+
+    rating_scores = [r.score for r in movie_ratings]
+    avg_rating = float(sum(rating_scores)) / len(rating_scores)
+
+    prediction = None
+
+    # Prediction code: only predict if the user hasn't rated it.
+
+    if (not current_user_rating) and current_user_id:
+        user = User.query.get(current_user_id)
+        if user:
+            prediction = user.predict_rating(movie)
+
+    # Either use the prediction or their real rating
+
+    if prediction:
+        # User hasn't scored; use our prediction if we made one
+        effective_rating = prediction
+
+    elif current_user_rating:
+        # User has already scored for real; use that
+        effective_rating = current_user_rating.score
+
+    else:
+        # User hasn't scored, and we couldn't get a prediction
+        effective_rating = None
+
+    # Get the eye's rating, either by predicting or using real rating
+
+    the_eye = User.query.filter_by(email="eye@gmail.com").one()
+    eye_rating = Rating.query.filter_by(user_id=the_eye.user_id, movie_id=movie.movie_id).first()
+
+    if eye_rating is None:
+        eye_rating = the_eye.predict_rating(movie)
+
+    else:
+        eye_rating = eye_rating.score
+
+    if eye_rating and effective_rating:
+        difference = abs(eye_rating - effective_rating)
+
+    else:
+        # We couldn't get an eye rating, so we'll skip difference
+        difference = None
+
+    # Depending on how different we are from the Eye, choose a message
+
+    BERATEMENT_MESSAGES = [
+        "I suppose you don't have such bad taste after all.",
+        "I regret every decision that I've ever made that has brought me" +
+            " to listen to your opinion.",
+        "Words fail me, as your taste in movies has clearly failed you.",
+        "That movie is great. For a clown to watch. Idiot.",
+        "Words cannot express the awfulness of your taste."
+    ]
+
+    if difference is not None:
+        beratement = BERATEMENT_MESSAGES[int(difference)]
+
+    else:
+        beratement = None
+
+    return render_template("movie_details.html", 
+                            movie=movie, 
+                            movie_ratings=movie_ratings,
+                            current_user_id=current_user_id,
+                            current_user_rating=current_user_rating,
+                            average=avg_rating,
+                            prediction=prediction,
+                            beratement=beratement)
 
 @app.route('/rate_movie', methods=["POST"])
 def rate_movie():
